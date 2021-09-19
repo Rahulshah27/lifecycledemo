829789a9f6fc06f14d3541e627ff42a617f6b112
Rahulshah27 <rahulshah0210@gmail.com>
2021-09-19 12:38:30 +0530

    initcommit-session3

---
+++ app/src/main/java/com/rahul/lifecycleexample/ActivityDialog.kt
@@ -0,0 +1,11 @@
+package com.rahul.lifecycleexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityDialog : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dialog)
+    }
+}
